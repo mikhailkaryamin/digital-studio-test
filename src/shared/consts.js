f1633f07f4139b1c9cc5a6d0c77c9c8926d014f4
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-20 15:45:28 +0300

    chore(consts): add ColorScheme, DEFAULT_GROUP, KEY_STORAGE_LINKS

---
+++ src/shared/consts.js
@@ -0,0 +1,16 @@
+const ColorScheme = {
+  DARK_BLUE: "#185abc",
+  CYAN_BLUE: "#1a73e8",
+  BLUE: "#e8eaed",
+  WHITE: "#ffffff",
+  RED: "#ff0303",
+};
+
+const DEFAULT_GROUP = "Все группы";
+const KEY_STORAGE_LINKS = "links";
+
+export {
+  KEY_STORAGE_LINKS,
+  DEFAULT_GROUP,
+  ColorScheme,
+};
