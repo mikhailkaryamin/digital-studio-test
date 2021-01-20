70b058e9148a0960262826d70b7be1af6b424d9d
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-20 15:44:34 +0300

    feat(LinkList): create component

---
+++ src/components/LinksList.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { array, func } from "prop-types";
+
+import Link from "./Link";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`;
+
+const STYLE_LINK = `
+  margin-bottom: 10px;
+`;
+
+const LinksList = ({ linksListData, onRemoveLink }) => {
+  return (
+    <Container>
+      {linksListData.map((item) => {
+        return (
+          <Link
+            link={item.link}
+            title={item.title}
+            id={item.id}
+            key={item.id}
+            addStyle={STYLE_LINK}
+            onRemoveLink={onRemoveLink}
+          />
+        );
+      })}
+    </Container>
+  );
+};
+
+LinksList.propTypes = {
+  linksListData: array.isRequired,
+  onRemoveLink: func.isRequired,
+};
+
+export default LinksList;
